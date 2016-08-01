c6c8d776aa8d95db10a76135890a031515add0bd
lujjjh <lujjjh@gmail.com>
2016-08-01 23:54:13 +0800

    Optimize multiple Han#ready() calls

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var expect = require('expect.js');
+var map = require('async/map');
 var Han = require('..');
 
 describe('Han', function () {
@@ -27,6 +28,25 @@ describe('Han', function () {
       expect(han.render('Hello世界！', function (han) {
         han.renderHWS();
       })).to.be('Hello<h-hws hidden=""> </h-hws>世界！');
+    });
+
+    it('should work with multiply calls', function (done) {
+      var testIn = ['Hello world', 'Hello，世界', '<em>Emph</em>'];
+      var testOut = [
+        "Hello world",
+        "Hello<h-char unicode=\"ff0c\" class=\"biaodian cjk bd-end bd-cop bd-jiya bd-hangable\"><h-inner>，</h-inner></h-char>世界",
+        "<em><h-word class=\"western\"><h-char class=\"alphabet latin\">E</h-char><h-char class=\"alphabet latin\">m</h-char><h-char class=\"alphabet latin\">p</h-char><h-char class=\"alphabet latin\">h</h-char></h-word></em>"
+      ];
+
+      var anotherHan = new Han();
+      map(testIn, function (content, callback) {
+        anotherHan.ready(function () {
+          callback(null, this.render(content));
+        });
+      }, function (err, results) {
+        expect(results).to.eql(testOut);
+        done();
+      });
     });
   });
 });
